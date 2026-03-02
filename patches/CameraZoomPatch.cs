ba275ce6a0514a8b72865a0897613e34fb55d6c7
minimusubi <minimusubi@users.noreply.github.com>
2026-03-01 20:56:22 -1000

    feat(CameraZoomPatch): add a camera mode that focuses only on local players

---
+++ patches/CameraZoomPatch.cs
@@ -0,0 +1,54 @@
+﻿using Cinemachine;
+using UnofficialPatches.utilities;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using static Cinemachine.CinemachineTargetGroup;
+
+namespace UnofficialPatches.patches {
+	/**
+	 * Adds a camera mode focused only on local players, toggleable with Left Control or Left Trigger.
+	 */
+	[HarmonyPatch(typeof(CameraManager))]
+	internal class CameraZoomPatch {
+		static Target[] originalTargets = null;
+
+		[HarmonyPatch("Update")]
+		[HarmonyPostfix]
+		static private void Update(CinemachineTargetGroup ___targetGroup) {
+			if (InputHelper.GetLeftTriggerDown() || InputHelper.GetLeftCtrlDown()) {
+				SwapCamera(___targetGroup);
+			}
+		}
+
+		static private void SwapCamera(CinemachineTargetGroup targetGroup) {
+			if (originalTargets == null) {
+				ActivateSoloCamera(targetGroup);
+			} else {
+				RestoreGroupCamera(targetGroup);
+			}
+		}
+
+		static private void ActivateSoloCamera(CinemachineTargetGroup targetGroup) {
+			MultiplayerManager manager = Singleton<MultiplayerManager>.Instance;
+			originalTargets = targetGroup.m_Targets;
+			List<Target> targets = [];
+
+			foreach (PlayerCharacter character in manager.localPlayerCharacters) {
+				targets.Add(new Target {
+					target = character.gameObject.GetComponent<Transform>(),
+					weight = 1,
+					radius = 0
+				});
+			}
+
+			targetGroup.m_Targets = [.. targets];
+		}
+
+		static private void RestoreGroupCamera(CinemachineTargetGroup targetGroup) {
+			targetGroup.m_Targets = originalTargets;
+			originalTargets = null;
+
+		}
+	}
+}
---
+++ utilities/InputHelper.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using Rewired;
+using UnityEngine;
+using static Rewired.Controller;
+
+namespace UnofficialPatches.utilities {
+	[HarmonyPatch]
+	internal class InputHelper {
+		static bool wasLeftTriggerPressed = false;
+		static bool isLeftTriggerDown = false;
+
+		[HarmonyPatch(typeof(CameraManager), "Update")]
+		[HarmonyPostfix]
+		private static void Update() {
+			bool isLeftTriggerPressed = false;
+			isLeftTriggerDown = false;
+
+			foreach (Joystick joystick in ReInput.controllers.Joysticks) {
+				foreach (Axis axis in joystick.Axes) {
+					if (axis.elementIdentifier.name != "Left Trigger") {
+						continue;
+					}
+
+					if (axis.value > 0.6f) {
+						isLeftTriggerPressed = true;
+						break;
+					}
+				}
+			}
+
+			if (isLeftTriggerPressed && !wasLeftTriggerPressed) {
+				isLeftTriggerDown = true;
+			}
+
+			wasLeftTriggerPressed = isLeftTriggerPressed;
+		}
+
+		internal static bool GetLeftTriggerDown() {
+			return isLeftTriggerDown;
+		}
+
+		internal static bool GetLeftCtrlDown() {
+			return Input.GetKeyDown(KeyCode.LeftControl);
+		}
+	}
+}
