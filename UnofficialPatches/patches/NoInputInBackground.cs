1e1de64b9d0c27b5d227fbb97eb8010e05f32e2c
minimusubi <minimusubi@users.noreply.github.com>
2026-08-28 04:28:28 -1000

    feat: prevent input when game is out of focus

---
+++ UnofficialPatches/patches/NoInputInBackground.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace UnofficialPatches.patches {
+	/**
+	 * Prevents player input while the game is out of focus.
+	 */
+	[HarmonyPatch(typeof(PlayerCharacter))]
+	internal class NoInputInBackground {
+		[HarmonyPatch("GetInput")]
+		[HarmonyPrefix]
+		private static bool GetInput() {
+			if (!Application.isFocused) {
+				// The application is in the background; skip processing inputs
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
